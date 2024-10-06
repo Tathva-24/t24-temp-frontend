5a1256ed818dd2fe4fd4e0bed355ee1a934f5dcb
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-10-06 23:33:41 +0530

    edited landing

--- t24-temp/src/components/ui/landing.tsx
+++ t24-temp/src/components/ui/landing.tsx
@@ -2,9 +2,9 @@ export default function Landing() {
   return (
     <>
       <div className={`flex flex-col justify-center items-center text-white font-kodeMono`}>
-        <h1 className="text-[120px]">TATHVA'24</h1>
-        <h3 className="text-[60px]">COMING SOON</h3>
-        <div className="text-[40px] flex justify-center items-center gap-10">
+        <h1 className="text-[8vw]">TATHVA'24</h1>
+        <h3 className="text-[4vw]">COMING SOON</h3>
+        <div className="px-7 w-full mt-10 text-[2.5vw] flex justify-between items-center gap-10">
           <p>24 OCT</p>
           <p>25 OCT</p>
           <p>26 OCT</p>
