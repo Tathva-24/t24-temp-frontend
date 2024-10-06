a4011c2b70060d8bfe8ef0015b241fa7bd065895
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-10-06 23:17:10 +0530

    created landing

--- t24-temp/src/app/page.tsx
+++ t24-temp/src/app/page.tsx
@@ -2,7 +2,12 @@
 import { motion } from "framer-motion";
 import { HeroHighlight, Highlight } from "../components/ui/hero-highlight";
 import Image from "next/image";
+import Landing from "@/components/ui/landing";
 
 export default function Home() {
-  return <HeroHighlight></HeroHighlight>;
+  return (
+    <HeroHighlight>
+      <Landing />
+    </HeroHighlight>
+  );
 }
---
+++ t24-temp/src/components/ui/landing.tsx
@@ -0,0 +1,15 @@
+export default function Landing() {
+  return (
+    <>
+      <div className={`flex flex-col justify-center items-center text-white font-kodeMono`}>
+        <h1 className="text-[120px]">TATHVA'24</h1>
+        <h3 className="text-[60px]">COMING SOON</h3>
+        <div className="text-[40px] flex justify-center items-center gap-10">
+          <p>24 OCT</p>
+          <p>25 OCT</p>
+          <p>26 OCT</p>
+        </div>
+      </div>
+    </>
+  );
+}
