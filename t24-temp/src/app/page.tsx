37ec04aec7f0c885aaa3fddf6feca909a12ebcff
arunnats <arunnats2004@gmail.com>
2024-10-06 21:50:28 +0530

    added acerternity

---
+++ t24-temp/src/app/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { motion } from "framer-motion";
+import { HeroHighlight, Highlight } from "../components/ui/hero-highlight";
+import Image from "next/image";
+
+export default function Home() {
+  return <HeroHighlight></HeroHighlight>;
+}
