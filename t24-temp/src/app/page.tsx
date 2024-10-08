0222adcd6f226f998df6c754610d92edf055ca60
arunnats <arunnats2004@gmail.com>
2024-10-08 09:06:38 +0530

    responsive

--- t24-temp/src/app/page.tsx
+++ t24-temp/src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { HeroHighlight } from "../components/ui/hero-highlight";
+import { Analytics } from "@vercel/analytics/react";
 import Landing from "@/components/ui/landing";
 
 export default function Home() {
   return (
     <HeroHighlight>
+      <Analytics />
       <Landing />
     </HeroHighlight>
   );
--- t24-temp/src/components/ui/landing.tsx
+++ t24-temp/src/components/ui/landing.tsx
@@ -4,9 +4,9 @@ export default function Landing() {
       <div
         className={`flex flex-col justify-center items-center text-white font-kodeMono`}
       >
-        <h1 className="text-[8vw]">TATHVA&#39;24</h1>
-        <h3 className="text-[4vw]">COMING SOON</h3>
-        <div className="px-7 w-full mt-10 text-[2.5vw] flex justify-between items-center gap-10">
+        <h1 className="text-[15vw] md:text-[8vw]">TATHVA&#39;24</h1>
+        <h3 className="text-[11vw] md:text-[4vw]">COMING SOON</h3>
+        <div className="px-7 w-full mt-10 text-[5vw] md:text-[2.5vw] flex justify-between items-center gap-10">
           <p>24 OCT</p>
           <p>25 OCT</p>
           <p>26 OCT</p>
