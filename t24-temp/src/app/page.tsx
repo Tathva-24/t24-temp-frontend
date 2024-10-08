51d81ac923f97a433c28f29af437c56771bb75f0
arunnats <arunnats2004@gmail.com>
2024-10-08 08:56:48 +0530

    removed nav

--- t24-temp/src/app/page.tsx
+++ t24-temp/src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import { HeroHighlight } from "../components/ui/hero-highlight";
-import { NavBar } from "../components/navbar/navbar";
 import Landing from "@/components/ui/landing";
 
 export default function Home() {
   return (
     <HeroHighlight>
-      <NavBar />
       <Landing />
     </HeroHighlight>
   );
