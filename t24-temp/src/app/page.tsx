b2dd0cda4fdb528f1a7f8e289596214a9bfc0849
arunnats <arunnats2004@gmail.com>
2024-10-08 09:09:42 +0530

    background

--- t24-temp/src/app/page.tsx
+++ t24-temp/src/app/page.tsx
@@ -2,12 +2,16 @@
 import { HeroHighlight } from "../components/ui/hero-highlight";
 import { Analytics } from "@vercel/analytics/react";
 import Landing from "@/components/ui/landing";
+import { div } from "framer-motion/client";
 
 export default function Home() {
   return (
-    <HeroHighlight>
-      <Analytics />
-      <Landing />
-    </HeroHighlight>
+    <div className="bg-black">
+      {" "}
+      <HeroHighlight>
+        <Analytics />
+        <Landing />
+      </HeroHighlight>
+    </div>
   );
 }
