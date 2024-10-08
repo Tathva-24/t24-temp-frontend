e2e7c09901e855d533f5b93b26866475fcd1e8ae
arunnats <arunnats2004@gmail.com>
2024-10-08 09:18:20 +0530

    tsx error

--- t24-temp/src/app/page.tsx
+++ t24-temp/src/app/page.tsx
@@ -2,7 +2,6 @@
 import { HeroHighlight } from "../components/ui/hero-highlight";
 import { Analytics } from "@vercel/analytics/react";
 import Landing from "@/components/ui/landing";
-import { div } from "framer-motion/client";
 
 export default function Home() {
   return (
