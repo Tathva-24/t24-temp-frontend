2cf6dddfc4bc75f76fd9779b8e34208a21157504
arunnats <arunnats2004@gmail.com>
2024-10-09 11:18:30 +0530

    whoever the fuck is writing css needs to go reevaluate their life choices. jsut stop

--- t24-temp/src/components/Navbar2/Navbar2.tsx
+++ t24-temp/src/components/Navbar2/Navbar2.tsx
@@ -1,8 +1,8 @@
-'use client';
-import React, { useState, useRef } from 'react';
-import styles from './navbar2.module.css';
-import Link from 'next/link';
-import { gsap } from 'gsap';
+"use client";
+import React, { useState, useRef } from "react";
+import styles from "./navbar2.module.css";
+import Link from "next/link";
+import { gsap } from "gsap";
 
 export default function Navbar2() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,34 +13,50 @@ export default function Navbar2() {
 
     if (!isOpen) {
       gsap.to(menuRef.current, {
-        height: '100vh', // Set height to full screen when open
+        height: "100vh", // Set height to full screen when open
         duration: 0.5,
-        ease: 'power2.out',
+        ease: "power2.out",
       });
     } else {
       gsap.to(menuRef.current, {
         height: 0, // Collapse the menu when closed
         duration: 0.5,
-        ease: 'power2.out',
+        ease: "power2.out",
       });
     }
   };
 
   return (
-    <div className={`${styles.main} w-screen z-20 absolute top-0 left-0 font-kodeMono `}>
+    <div
+      className={`${styles.main} w-screen z-20 absolute top-0 left-0 font-kodeMono `}
+    >
       <div className={`${styles.navbar}`}>
         {/* Desktop Menu */}
-        <div className={`${styles.desktopMenu} flex justify-evenly items-center w-screen `}>
-          <Link href='/' className={`${styles.navItem} text-white flex items-center justify-center hover:scale-110 transition-all h-10 w-max px-6`}>
+        <div
+          className={`${styles.desktopMenu} mt-8 mx-auto w-[70vw] flex justify-evenly items-center w-screen `}
+        >
+          <Link
+            href="/"
+            className={`${styles.navItem} text-white flex items-center justify-center hover:scale-110 transition-all h-10 w-max px-6`}
+          >
             Home
           </Link>
-          <Link href='/workshop' className={`${styles.navItem} text-white flex items-center justify-center hover:scale-110 transition-all h-10 w-max px-6 duration-300`}>
+          <Link
+            href="/workshop"
+            className={`${styles.navItem} text-white flex items-center justify-center hover:scale-110 transition-all h-10 w-max px-6 duration-300`}
+          >
             Workshop
           </Link>
-          <Link href='/lecture' className={`${styles.navItem} text-white flex items-center justify-center hover:scale-110 transition-all h-10 w-max px-6 duration-300`}>
+          <Link
+            href="/lecture"
+            className={`${styles.navItem} text-white flex items-center justify-center hover:scale-110 transition-all h-10 w-max px-6 duration-300`}
+          >
             Lecture
           </Link>
-          <Link href='/contact' className={`${styles.navItem} text-white flex items-center justify-center hover:scale-110 transition-all h-10 w-max px-6 duration-300`}>
+          <Link
+            href="/contact"
+            className={`${styles.navItem} text-white flex items-center justify-center hover:scale-110 transition-all h-10 w-max px-6 duration-300`}
+          >
             Contact
           </Link>
         </div>
@@ -53,17 +69,32 @@ export default function Navbar2() {
         </div>
 
         {/* Mobile Menu */}
-        <div ref={menuRef} className={`${styles.menu} h-screen w-screen flex flex-col justify-evenly items-center font-kodeMono `}>
-          <Link href='/' className={`${styles.navItem} text-white hover:bg-slate-50 hover:bg-opacity-30 h-full flex-grow-0 flex items-center justify-center transition-all w-full px-6`}>
+        <div
+          ref={menuRef}
+          className={`${styles.menu} h-screen w-screen flex flex-col justify-evenly items-center font-kodeMono `}
+        >
+          <Link
+            href="/"
+            className={`${styles.navItem} text-white hover:bg-slate-50 hover:bg-opacity-30 h-full flex-grow-0 flex items-center justify-center transition-all w-full px-6`}
+          >
             Home
           </Link>
-          <Link href='/workshop' className={`${styles.navItem} text-white hover:bg-slate-50 hover:bg-opacity-30 h-full flex-grow-0 flex items-center justify-center transition-all w-full px-6 duration-300`}>
+          <Link
+            href="/workshop"
+            className={`${styles.navItem} text-white hover:bg-slate-50 hover:bg-opacity-30 h-full flex-grow-0 flex items-center justify-center transition-all w-full px-6 duration-300`}
+          >
             Workshop
           </Link>
-          <Link href='/lecture' className={`${styles.navItem} text-white hover:bg-slate-50 hover:bg-opacity-30 h-full flex-grow-0 flex items-center justify-center transition-all w-full px-6 duration-300`}>
+          <Link
+            href="/lecture"
+            className={`${styles.navItem} text-white hover:bg-slate-50 hover:bg-opacity-30 h-full flex-grow-0 flex items-center justify-center transition-all w-full px-6 duration-300`}
+          >
             Lecture
           </Link>
-          <Link href='/contact' className={`${styles.navItem} text-white hover:bg-slate-50 hover:bg-opacity-30 h-full flex-grow-0 flex items-center justify-center transition-all w-full px-6 duration-300`}>
+          <Link
+            href="/contact"
+            className={`${styles.navItem} text-white hover:bg-slate-50 hover:bg-opacity-30 h-full flex-grow-0 flex items-center justify-center transition-all w-full px-6 duration-300`}
+          >
             Contact
           </Link>
         </div>
--- t24-temp/src/components/ui/landing.tsx
+++ t24-temp/src/components/ui/landing.tsx
@@ -1,21 +1,22 @@
 import Navbar2 from "../Navbar2/Navbar2";
 
-export default function Landing( ) {
+export default function Landing() {
   return (
     <>
       <div
         className={`h-screen w-screen flex flex-col justify-between text-white font-kodeMono`}
-      > <Navbar2 />
+      >
+        {" "}
+        <Navbar2 />
         <div className="contatiner flex flex-col justify-center items-center text-white font-kodeMono h-full">
-        <h1 className="text-[15vw] md:text-[8vw]">TATHVA&#39;24</h1>
+          <h1 className="text-[15vw] md:text-[8vw]">TATHVA&#39;24</h1>
           <h3 className="text-[11vw] md:text-[4vw]">COMING SOON</h3>
           <div className="px-7 mt-10 text-[5vw] md:text-[2.5vw] flex justify-between items-center gap-10">
-            <p className='mx-3'>24 OCT</p>
-            <p className='mx-3'>25 OCT</p>
-            <p className='mx-3'>26 OCT</p>
+            <p className="mx-3">24 OCT</p>
+            <p className="mx-3">25 OCT</p>
+            <p className="mx-3">26 OCT</p>
           </div>
         </div>
-        
       </div>
     </>
   );
