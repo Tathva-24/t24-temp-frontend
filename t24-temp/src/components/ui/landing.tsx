73fb3f5862e93cbc91ede67a15f78ca40ecd503f
arunnats <arunnats2004@gmail.com>
2024-10-10 00:45:34 +0530

    dates

--- t24-temp/src/components/ui/landing.tsx
+++ t24-temp/src/components/ui/landing.tsx
@@ -11,9 +11,9 @@ export default function Landing() {
           <h1 className="text-[15vw] md:text-[8vw]">TATHVA&#39;24</h1>
           <h3 className="text-[11vw] md:text-[4vw]">COMING SOON</h3>
           <div className="px-7 mt-10 text-[5vw] md:text-[2.5vw] flex justify-between items-center gap-10">
-            <p className="mx-3">24 OCT</p>
             <p className="mx-3">25 OCT</p>
             <p className="mx-3">26 OCT</p>
+            <p className="mx-3">27 OCT</p>
           </div>
         </div>
       </div>
