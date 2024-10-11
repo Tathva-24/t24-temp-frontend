0a8ddcae6d7c42350977d2ee1c4fff8d7fe29047
arunnats <arunnats2004@gmail.com>
2024-10-11 15:47:08 +0530

    [fix] inshallah moment

--- t24-temp/src/components/ui/landing.tsx
+++ t24-temp/src/components/ui/landing.tsx
@@ -11,14 +11,17 @@ export default function Landing() {
         <Navbar2 />
         <div className="contatiner flex flex-col justify-center items-center text-white font-kodeMono h-full">
           <h1 className="text-[15vw] md:text-[8vw]">TATHVA&#39;24</h1>
-          <h3 className="text-[11vw] md:text-[4vw] mb-2">COMING SOON</h3>
-          <Link href="/workshop" className={styles.navItem}>
-            Register Here
-          </Link>
+          <h3 className="text-[11vw] md:text-[4vw]">COMING SOON</h3>
+
           <div className="px-7 mt-5 text-[3.5vw] md:text-[2.5vw] flex justify-between items-center gap-10">
             <p className="mx-3">25 OCT</p>
             <p className="mx-3">26 OCT</p>
             <p className="mx-3">27 OCT</p>
+          </div>
+          <div className="mt-5">
+            <Link href="/workshop" className={styles.navItem}>
+              Register Here
+            </Link>
           </div>
         </div>
       </div>
