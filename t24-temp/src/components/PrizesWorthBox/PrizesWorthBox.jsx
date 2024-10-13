7e25489de2c5dede85a330caad67c795a0af05e7
Rohith <rohithofficial10@gmail.com>
2024-10-14 00:42:02 +0530

    finished

--- t24-temp/src/components/PrizesWorthBox/PrizesWorthBox.jsx
+++ t24-temp/src/components/PrizesWorthBox/PrizesWorthBox.jsx
@@ -1,26 +1,27 @@
-import React from 'react'   
-import "./main.css"
-const Main = () => {
-  return (
-    <div className="PrizesWorthBoxflex flex-col justify-center py-8 px-8">
-        <div className="head text-8xl text-white flex justify-center border-b-2 py-8">Prizes Worth</div>
-        <div className="bod py-8 pb-0 text-white text-2xl flex justify-between">
-            <div className="date">
-                <span className="">Date: </span>
-                <span className="date_day">28/29 Oct</span>
-            </div>
-            <div className="rt whitespace-normal w-[44%] flex justify-between">
-                <div className="venue font-Presstart w-[30%]">Venue:</div>
-                <span className="place font-Presstart text-right inline-block w-[60%]">Football ground  </span>
-            </div>
-        </div>
-        <div className="flex space-x-4  justify-center">
-            <div className="w-8 h-8 border-2 border-white"></div>
-            <div className="w-8 h-8 border-2 border-white"></div>
-            <div className="w-8 h-8 border-2 border-white"></div>
-        </div>
-    </div>
-  )
-}
+import React from 'react';
+import styles from './PrizesWorthBox.module.css';
 
-export default PrizesWorthBox
+const PrizesWorthBox = () => {
+    return (
+        <div className={`${styles.prize} flex flex-col justify-center py-8 px-8`}>
+            <div className="head text-8xl text-white flex justify-center border-b-2 py-8 font-pricedown">Prizes Worth</div>
+            <div className="bod py-8 pb-0 text-white txt-2xl flex justify-between">
+                <div className="date">
+                    <span className="font-presstart">Date:</span>
+                    <span className="font-presstart" style={{ color: 'var(--neon)' }}>28/29 Oct</span>
+                </div>
+                <div className="rt whitespace-normal w-[44%] flex justify-between">
+                    <span className="venue font-presstart w-[30%]">Venue:</span>
+                    <span className="place font-presstart text-right inline-block w-[60%]" style={{ color: 'var(--neon)' }}>Football ground</span>
+                </div>
+            </div>
+            <div className="flex justify-center space-x-1">
+                <div className="h-4 w-4 border-2 border-white"></div>
+                <div className="h-4 w-4 border-2 border-white"></div>
+                <div className="h-4 w-4 border-2 border-white"></div>
+            </div>
+        </div>
+    );
+};
+
+export default PrizesWorthBox;
