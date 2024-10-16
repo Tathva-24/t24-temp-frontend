489846bb6ca0147645664e1fdb1fb34a83bc8c8a
arunnats <arunnats2004@gmail.com>
2024-10-17 02:17:00 +0530

    [fix] changed background for events page

--- t24-temp/src/app/events/page.tsx
+++ t24-temp/src/app/events/page.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import Card from "../../components/Card_img2/Card_img2.jsx";
 import styles from "./page.module.css";
 import Navbar2Black from "@/components/Navbar2Black/Navbar2Black";
+import { HeroHighlight } from "../../components/ui/hero-highlight";
 
 const CMS_URL = "https://cms.tathva.org";
 const CMS_API_TOKEN =
@@ -38,32 +39,34 @@ export default function Event() {
 
   return (
     <>
-      <Navbar2Black />
-      <div className={`${styles.container} font-kodeMono mt-20 `}>
-        <h1 className={styles.title}> COMPETITIONS</h1>
+      <HeroHighlight>
+        <Navbar2Black />
+        <div className={`${styles.container} font-kodeMono mt-20 `}>
+          <h1 className={styles.title}> COMPETITIONS</h1>
 
-        {/* Container for the workshops */}
-        <div className={styles.cardContainer}>
-          {events.map((events, index) =>
-            events.coverImage?.url &&
-            events.title &&
-            events.description &&
-            events.regFee &&
-            events.eventDate &&
-            events.regLink ? (
-              <Card
-                key={index}
-                image={CMS_URL + events.coverImage.url}
-                heading={events.title}
-                description={events.description}
-                price={"₹" + events.regFee}
-                date={events.eventDate}
-                link={events.regLink}
-              />
-            ) : null
-          )}
+          {/* Container for the workshops */}
+          <div className={styles.cardContainer}>
+            {events.map((events, index) =>
+              events.coverImage?.url &&
+              events.title &&
+              events.description &&
+              events.regFee &&
+              events.eventDate &&
+              events.regLink ? (
+                <Card
+                  key={index}
+                  image={CMS_URL + events.coverImage.url}
+                  heading={events.title}
+                  description={events.description}
+                  price={"₹" + events.regFee}
+                  date={events.eventDate}
+                  link={events.regLink}
+                />
+              ) : null
+            )}
+          </div>
         </div>
-      </div>
+      </HeroHighlight>
     </>
   );
 }
